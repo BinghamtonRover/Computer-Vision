955f7dc4e2027d731cc07225b45e768d8735497f
aidan <aidanahram1@gmail.com>
2024-12-25 00:08:08 -0500

    progress

--- lib/analyzer.py
+++ lib/analyzer.py
@@ -1,6 +1,14 @@
 import cv2
+from ultralytics import YOLO
 
 class ImageAnalyzer:
+  def __init__(self):
+    self.model = YOLO("trained-model.pt")
+                
   def has_mallet(self, frame: cv2.Mat) -> bool:
     # TODO: Check for mallets
+    results = self.model(frame)
+    print("CALLING HAS MALLET")
+    for result in results:
+      result.show()
     return False
