b716097f398bd0131470d33736766f1f174d83b7
aadij0415@gmail.com <aadij0415@gmail.com>
2025-01-22 17:41:04 -0500

    removed prints

--- bin/analyze.py
+++ bin/analyze.py
@@ -4,16 +4,12 @@ import cv2
 import os
 
 from lib.analyzer import ImageAnalyzer
-analyzer = ImageAnalyzer()
 
-
-def test(): 
+def main(): 
+  analyzer = ImageAnalyzer()
   for file in os.listdir("test_images"): 
-
     filename, _ = file.split(".")
     if not filename: continue 
-    """ print(f"file is: {file}")
-    print(f"!filename is:{filename}")     """             # chair_yes.jpg --> chair_yes, jpg
     background, has_mallet = filename.split("_")  # chair_yes --> chair, yes
     has_mallet = has_mallet == "yes"              # yes --> True
     frame = cv2.imread(f"test_images/{file}")
@@ -23,28 +19,5 @@ def test():
     else: 
       print(f"{background} image failed: Analyzer detected {result} instead of {has_mallet}")
 
-
-def main():
-  print("Loading model...")
-  
-  no_mallet = cv2.imread("no_mallet.jpg")
-  yes_mallet = cv2.imread("yes_Mallet.jpg")
-
-  print("Analyzing no_mallet... ", end = "")
-  found_mallet = analyzer.has_mallet(no_mallet)
-  if found_mallet:
-    print("Failed\nFound mallet when there is no mallet")
-    return
-  print("Success!")
-
-  print("Analyzing yes_mallet... ", end = "")
-  found_mallet = analyzer.has_mallet(yes_mallet)
-  if not found_mallet:
-    print("Failed\nCould not detect mallet")
-    return
-  print("Success!")
-
 if __name__ == "__main__":
   main()
-  test()
-
--- lib/analyzer.py
+++ lib/analyzer.py
@@ -6,13 +6,13 @@ class ImageAnalyzer:
     self.model = YOLO("trained-model.pt", verbose=False)
 
   def has_mallet(self, frame: cv2.Mat, confidence = 0.5) -> bool:
-    print(type(frame))
     results = self.model.predict(frame, stream=True, verbose=False)
     for result in results:
       json = result.summary()
       if not json: return False
       object = json[0]
       is_mallet = object['name'] == "mallet" and object['confidence'] > confidence
+      print("Confidence: " + str(object['confidence']))
       if is_mallet: return True
     else:
       return False
